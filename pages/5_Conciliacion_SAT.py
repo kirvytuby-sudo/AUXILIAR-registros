eeecd753e31ea0bc92001c10189aa75d565388d7
kirvytuby-sudo <kirvytuby@gmail.com>
2026-07-05 15:38:25 -0600

    Add files via upload

--- pages/5_Conciliacion_SAT.py
+++ pages/5_Conciliacion_SAT.py
@@ -11,7 +11,7 @@ st.set_page_config(
     page_title="Conciliación SAT",
     page_icon="🔗",
     layout="wide",
-    initial_sidebar_bar="expanded",
+    initial_sidebar_state="expanded",
 )
 
 st.markdown("""
